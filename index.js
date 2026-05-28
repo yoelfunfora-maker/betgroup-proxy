fa03dd9efb95097f4fdd10d6ab7fa7f26b13943d
yoelfunfora-maker <yoelfunfora@gmail.com>
2026-05-27 23:22:07 -0400

    Fix: añadir cors y ajustar start script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const admin = require('firebase-admin');
 
 // Inicializar Firebase Admin
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const db = admin.database();
 
